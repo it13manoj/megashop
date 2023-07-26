85303a2182a8c6ee215000785ab3c213d95d911c
manoj sharma <it13manoj@gmail.com>
2023-07-26 08:31:13 +0530

    update

--- src/helper/APIInfo.js
+++ src/helper/APIInfo.js
@@ -1,5 +1,5 @@
-const SERVER_LINK = "http://api.mega-shop.co.in/api";
-const SERVER_IMAGES = "http://api.mega-shop.co.in";
+const SERVER_LINK = "https://api.mega-shop.co.in/api";
+const SERVER_IMAGES = "https://api.mega-shop.co.in/";
 export const REQUEST_TYPE = {
   GET: "GET",
   POST: "POST",
